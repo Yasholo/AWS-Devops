0fd77034a87c4bfd3636ac8856a17e2aa68ca899
Yasholo <yashk5635@gmail.com>
2024-05-03 13:38:23 +0530

    files updated

--- AWS-Projects/AWS-Continuous-Integration-Demo/simple-python-app/start_container.sh
+++ AWS-Projects/AWS-Continuous-Integration-Demo/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull yasholo/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 yasholo/simple-python-flask-app
