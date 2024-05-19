fa92f8aa21455fa430ef8884144a352dab63f981
Yasholo <yashk5635@gmail.com>
2024-05-19 19:55:04 +0530

    updates and gitignore

--- AWS-Projects/AWS-terraform-multi-tier-architecture/modules/monitoring/cloudwatch.tf
+++ AWS-Projects/AWS-terraform-multi-tier-architecture/modules/monitoring/cloudwatch.tf
@@ -1,6 +1,6 @@
 # Define AWS provider
 provider "aws" {
-  region = "us-east-1"
+  region = var.region_name
 }
 
 # CloudWatch Log Group for application logs
@@ -16,4 +16,7 @@ resource "aws_cloudwatch_log_group" "system_logs" {
 }
 
 # CloudWatch Log Group for security logs
-resource "aws_cloudwatch_log_group" "security
+resource "aws_cloudwatch_log_group" "security_logs" {
+  name              = "/security_logs"
+  retention_in_days = 30
+}
