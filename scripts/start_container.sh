f93b5141e888f3d3174913b4d51d0e7b0c77d76c
Yasholo <yashk5635@gmail.com>
2024-06-13 13:24:11 +0530

    moved to different repo

--- scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull yasholo/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 yasholo/simple-python-flask-app
--- scripts/stop_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# Stop the running container (if any)
-echo "Hi"
-fuser -n tcp -k 5000
